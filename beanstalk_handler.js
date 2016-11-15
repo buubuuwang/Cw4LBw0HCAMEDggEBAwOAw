f92cfa1b32ccfe46d66ef66d64d366dfa76cbc31
glenn <buubuuwang@gmail.com>
2016-11-15 09:59:44 +0800

    1) first worker

---
+++ beanstalk_handler.js
@@ -0,0 +1,85 @@
+(function() {
+	var client = require('beanstalk_client').Client;
+	var BeanstalkHandler = (function() {
+		function BeanstalkHandler(server) {
+			this.server = server;
+		}
+		BeanstalkHandler.prototype.put_job = function(tubes, priority, delay, ttr, payload) {
+			return new Promise(function(resolve, reject){
+				return client.connect(this.server, function(err, conn) {
+					if (err) {
+						return reject('Error connecting: ' + err);
+					} else {
+						var job_data = payload?payload:{
+							type: 'currency_exchagne_rate',
+							retries:0,
+							data: {
+								"from": "HKD",
+								"to": "USD"
+							}
+						};
+						this.connection = conn;
+						conn.put(priority, delay, ttr, JSON.stringify(job_data), function(err, job_id) {
+							if (err) return reject(err);
+							console.log('put the job success successfully: ' + job_id);
+							resolve(job_data);
+						});
+					};
+				});
+			})
+		}
+		BeanstalkHandler.prototype.get_job = function(tubes) {
+			return new Promise(function(resolve, reject){
+				return client.connect(this.server, function(err, conn) {
+					if (err) {
+						return reject('Error connecting: ' + err);
+					} else {
+						this.connection = conn;
+						this.connection.watch(tubes, function(err) {
+							this.connection.reserve(function(err, job_id, data) {
+								if(err) return reject(err);
+								console.log('Consumer got job: ' + job_id);
+								console.log('job data: ' + data);
+								var job_data = JSON.parse(data);
+								job_data['job_id'] = job_id;
+								resolve(job_data);
+							});
+						});
+					};
+				});
+			})
+		}
+		BeanstalkHandler.prototype.destroy_job = function(job_id,payload) {
+			return new Promise(function(resolve, reject){
+				this.connection.destroy(job_id, function(err) {
+					if(err) return reject(new Error(err));
+					resolve(payload);
+				});
+			})
+		}
+		BeanstalkHandler.prototype.bury_job = function(job_id) {
+			return new Promise(function(resolve, reject){
+				this.connection.bury(job_id,client.LOWEST_PRIORITY, function(err) {
+					if(err) return reject(new Error(err));
+					resolve(job_id);
+				});
+			})
+		}
+		BeanstalkHandler.prototype.release_job = function(job_id) {
+			return new Promise(function(resolve, reject){
+				this.connection.release(job_id,client.LOWEST_PRIORITY,function(err) {
+					if(err) return reject(new Error(err));
+					resolve(job_data);
+				});
+			})
+		}
+		BeanstalkHandler.prototype.stop = function(job_id) {
+			return new Promise(function(resolve, reject){
+				return this.connection.end();
+			})
+		}
+		return BeanstalkHandler;
+	})();
+	exports.BeanstalkHandler = BeanstalkHandler;
+}).call(this);
+
---
+++ data_handler.js
@@ -0,0 +1,41 @@
+'use strict'
+var request = require("request");
+var MongoClient = require('mongodb').MongoClient;
+var URL = 'mongodb://localhost:27017/exrates'
+
+var hkd_to_usd;
+exports.hkd_to_usd = hkd_to_usd = function (payload) {
+	var from = payload['data']['from'] || null;
+	var to = payload['data']['to'] || null;
+	return new Promise(function(resolve, reject){
+		request({
+			url: "http://api.fixer.io/latest?base="+from,
+			method: "GET"
+		}, function(error, response, body) {
+			if (error || !body) {
+				return reject(payload);
+			}
+			var rate = JSON.parse(body)['rates'][to];
+			//round off to 2 decmicals in STRING type
+			payload['rate'] = rate.toFixed((2));
+			//timestamp                            
+			payload['create_at'] = new Date();
+			resolve(payload);
+		});
+	})
+}
+
+var save_to_mongodb;
+exports.save_to_mongodb = save_to_mongodb = function (payload){
+	return new Promise(function(resolve, reject){
+		MongoClient.connect(URL, function(err, db) {
+			if (err) reject(err);
+			var collection = db.collection('hkd_to_usd')
+			collection.insert(payload, function(err, result) {
+				if(err) return reject(new Error(err));
+				resolve(payload);
+				return db.close()
+			})
+		})
+	});
+}
